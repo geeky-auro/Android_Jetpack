4ac3683403b74468259e387fc6b6ee87e4f8e777
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-04-14 00:58:44 +0530

    Implemented On Pinch Zoom and onClickFocus ;)

--- CameraX/app/src/main/java/com/aurosaswat/camerax/MainActivity.kt
+++ CameraX/app/src/main/java/com/aurosaswat/camerax/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.ImageCapture
 import androidx.camera.video.Recorder
 import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
@@ -17,19 +16,17 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import android.widget.Toast
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.core.Preview
-import androidx.camera.core.CameraSelector
 import android.util.Log
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.video.FallbackStrategy
-import androidx.camera.video.MediaStoreOutputOptions
-import androidx.camera.video.Quality
-import androidx.camera.video.QualitySelector
-import androidx.camera.video.VideoRecordEvent
-import androidx.core.content.PermissionChecker
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.View
+import android.view.View.OnTouchListener
+import androidx.camera.core.*
+import androidx.camera.core.impl.PreviewConfig
+import androidx.camera.extensions.internal.PreviewConfigProvider
+import androidx.lifecycle.Observer
 import com.aurosaswat.camerax.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -48,6 +45,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+//        Check for Permisssions, if everything is granted then allow the camera to click the picture
 
         if (allPermissionsGranted()){
             startCamera()
@@ -78,8 +77,7 @@ class MainActivity : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
 
-            imageCapture = ImageCapture.Builder()
-                .build()
+            imageCapture = ImageCapture.Builder().build()
 
             // Select back camera as a default
 
@@ -96,8 +94,93 @@ class MainActivity : AppCompatActivity() {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
+                val camera = cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture,imageAnalzer)
+
+                /**
+                 * Enable Torch of the Camera ;_
+                 *  Reference: https://proandroiddev.com/android-camerax-tap-to-focus-pinch-to-zoom-zoom-slider-eb88f3aa6fc6
+                 *
+                // Getting the CameraControl instance from the camera
+
+
+                val enableTorchLF:ListenableFuture<Void> = cameraControl.enableTorch(true)
+                enableTorchLF.addListener({
+                try{
+                enableTorchLF.get()
+                // At this point, the torch has been successfully enabled
+                }catch (exception:Exception){
+                //  Handle any potential Error ;)
+                }
+                },cameraExecutor)
+                /* Executor where the runnable callback code is run */
+                 * */
+
+
+                // Getting the CameraInfo instance from the camera
+                val cameraInfo=camera.cameraInfo
+                val cameraControl=camera.cameraControl
+                viewBinding.viewFinder.setOnTouchListener(OnTouchListener{
+                    view:View,motionEvent:MotionEvent ->
+                    when(motionEvent.action){
+                        MotionEvent.ACTION_DOWN-> return@OnTouchListener true
+                        MotionEvent.ACTION_UP->{
+                            val factory=viewBinding.viewFinder.meteringPointFactory
+
+                            val point=factory.createPoint(motionEvent.x,motionEvent.y)
+
+                            val action=FocusMeteringAction.Builder(point).build()
+
+                            cameraControl.startFocusAndMetering(action)
+
+                            return@OnTouchListener true
+
+                        }
+                        else -> return@OnTouchListener false
+                    }
+                })
+
+                val listener=object :ScaleGestureDetector.SimpleOnScaleGestureListener(){
+                    override fun onScale(detector: ScaleGestureDetector): Boolean {
+                        // Get the camera's current zoom ratio
+                        val currentZoomRatio=cameraInfo.zoomState.value?.zoomRatio?:0F
+
+                        // Get the pinch gesture's scaling factor
+                        val delta=detector.scaleFactor
+
+                        // Update the camera's zoom ratio. This is an asynchronous operation that returns
+                        // a ListenableFuture, allowing you to listen to when the operation completes.
+                        cameraControl.setZoomRatio(currentZoomRatio*delta)
+
+                        return true
+                    }
+                }
+
+                val scaleGestureDetector=ScaleGestureDetector(this,listener)
+
+//                Attach the pinch gesture listener to the viewFinder
+
+                viewBinding.viewFinder.setOnTouchListener { v, event ->
+                    scaleGestureDetector.onTouchEvent(event)
+                    return@setOnTouchListener true
+                }
+
+                /**
+                 *
+                 * CameraInfo provides most of the above information wrapped in LiveData, this allows you to continually observe their changes.
+                 * If you only need the current value of one of them, you can use LiveData.getValue().
+                 *
+                 * */
+
+//                // Use the CameraInfo instance to observe the zoom state
+////                cameraInfo.zoomState.observe(this, Observer {
+////                    // Use the zoom state to retireve information about the zoom ratio, etc
+////                    val currentZoomRatio=it.zoomRatio
+////                })
+
+
+
+
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -145,10 +228,10 @@ class MainActivity : AppCompatActivity() {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
 
-                override fun
-                        onImageSaved(output: ImageCapture.OutputFileResults){
+                override fun onImageSaved(output: ImageCapture.OutputFileResults){
                     val msg = "Photo capture succeeded: ${output.savedUri}"
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
                     Log.d(TAG, msg)
                 }
             }
@@ -215,6 +298,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 
 
 }
