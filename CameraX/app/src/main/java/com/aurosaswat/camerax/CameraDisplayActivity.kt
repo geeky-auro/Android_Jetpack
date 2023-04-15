972749aeeca2ffdd4eb1b0d642a37079f47d2d62
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-04-15 21:26:23 +0530

    Cropping Implemented :'

--- CameraX/app/src/main/java/com/aurosaswat/camerax/CameraDisplayActivity.kt
+++ CameraX/app/src/main/java/com/aurosaswat/camerax/CameraDisplayActivity.kt
@@ -2,16 +2,22 @@ package com.aurosaswat.camerax
 
 import android.R.attr
 import android.content.ContentResolver
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.aurosaswat.camerax.databinding.ActivityCameraDisplayBinding
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStream
 
 
@@ -33,11 +39,34 @@ class CameraDisplayActivity : AppCompatActivity() {
 
         viewBinding.button.setOnClickListener{
             val cropped = viewBinding.cropImageView.getCroppedImage()
-            val bs=ByteArrayOutputStream()
-            cropped?.compress(Bitmap.CompressFormat.PNG,50,bs)
-            val intent = Intent(this, DisplayCroppedPhoto::class.java)
-            intent.putExtra("BitmapImage", bs.toByteArray())
-            startActivity(intent)
+//            val bs=ByteArrayOutputStream()
+//            cropped?.compress(Bitmap.CompressFormat.PNG,50,bs)
+
+
+//            getDir() method is used to get or create a directory inside the app's internal storage space.
+            var cw=ContextWrapper(applicationContext)
+            var directory=cw.getDir("CroppedImg", Context.MODE_PRIVATE)
+            if(!directory.exists()){
+                directory.mkdir()
+            }
+
+            var myPath= File(directory,"cropped.png")
+            var fos:FileOutputStream?=null
+            try{
+                fos=FileOutputStream(myPath)
+                Toast.makeText(CameraDisplayActivity@this,"$myPath",Toast.LENGTH_SHORT).show()
+                cropped?.compress(Bitmap.CompressFormat.PNG,100,fos)
+                fos.close()
+
+            }catch (e:Exception){
+                Log.d("SAVE_IMAGE",e.message,e)
+            }
+            finally {
+                val intent = Intent(this, DisplayCroppedPhoto::class.java)
+                intent.putExtra("BitmapImage", myPath)
+                startActivity(intent)
+            }
+
         }
 
 
--- CameraX/app/src/main/java/com/aurosaswat/camerax/DisplayCroppedPhoto.kt
+++ CameraX/app/src/main/java/com/aurosaswat/camerax/DisplayCroppedPhoto.kt
@@ -2,11 +2,14 @@ package com.aurosaswat.camerax
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.aurosaswat.camerax.databinding.ActivityDisplayCroppedPhotoBinding
+import com.bumptech.glide.Glide
+import java.io.File
 
 
 class DisplayCroppedPhoto : AppCompatActivity() {
@@ -19,9 +22,10 @@ class DisplayCroppedPhoto : AppCompatActivity() {
         setContentView(viewBinding.root)
 
         if (intent.hasExtra("BitmapImage")){
-            var bitmap=BitmapFactory.decodeByteArray(
-                intent.getByteArrayExtra("byteArray"),0, intent.getByteArrayExtra("byteArray")!!.size)
-            viewBinding.imageView.setImageBitmap(bitmap)
+            val imageUri =intent.getStringExtra("BitmapImage")
+            Glide.with(this)
+                .load(Uri.fromFile(File(imageUri)))
+                .into(viewBinding.imageView)
         }
 
 
